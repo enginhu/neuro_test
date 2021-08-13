f24c8cb6873226b69e1af438ac782f64b2b058b5
engin <engin@ubuntu.home>
2021-08-13 14:38:51 +0300

    removed api key from config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
     MKAD_CENTER = [37.612, 55.71]
     MKAD_COORDINATES = [(37.329, 37.895),(55.503, 55.917)]
-    API_KEY = "8c53cc8d-2f29-491d-9fc5-3e4f4ea0dc67"
+    API_KEY = "YOUR_API_KEY_HERE"
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'very-very-secret-key-123789456'
     ERROR_MESSAGE = "Address not found or wrong address"
     TARGET_IN_MKAD_MESSAGE = "Location is inside of Moscow Ring Road."
